4bebd5a53d5880351f6d4714181ba7576041c78e
Me <fenton@bookingbug.com>
2017-11-16 16:42:34 +0000

    add data bas and crash if use does more than 11 POSTs WIP

--- app.rb
+++ app.rb
@@ -2,17 +2,60 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/formkeeper'
 require 'pry'
+require 'pstore'
+
+store = PStore.new('my_database.pstore') # Create or Load my_database.pstore file
+store.transaction do
+  store[:nom] = [ "Name" ]
+  store[:booking] = [ "Bookings" ]
+  store[:ip] = [ "I.P Address" ]
+  store[:secret] = [ "Time." ]
+ # Save Changes
+  store.commit
+
+
+  # Read more at the docs at http://ruby-doc.org/stdlib-2.2.3/libdoc/pstore/rdoc/PStore.html
+end
+$times_posted = 0
 
 get '/' do
+$times_posted += 1
+if $times_posted > 13
+store.transaction{store[:secret]}
+sleep 5
+exit 1
+end
   erb :index
 end
 
 
 post '/' do
+$times_posted += 1
+if $times_posted > 11
+exit 1
+end
 @params = params
 unless params["name"].to_s.include?('@')
 params["name"] = 'That is not an email address!'
+else
+store.transaction do
+  store[:nom] << params["name"].to_s 
+  store[:booking] << params["booking"].to_s
+  store[:ip] << '1.1.1.1'
+  store[:secret] << Time.now.to_s
+ # Save Changes
+  store.commit
+
+
+  # Read more at the docs at http://ruby-doc.org/stdlib-2.2.3/libdoc/pstore/rdoc/PStore.html
 end
+params["name"] = ''
+
+
+
+
+end
+@times_posted = $times_posted + 1
 erb :index
 #"My name is #{params[:name]}, and I love #{params[:favorite_food]}"
 
