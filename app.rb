1cae6aac89bd8905b032d12a1abdb79b6e298db0
Marius <jorgensen.marius@gmail.com>
2017-11-22 16:03:03 +0000

    naugty

--- app.rb
+++ app.rb
@@ -3,8 +3,10 @@ require 'sinatra'
 require 'sinatra/formkeeper'
 require 'pry'
 require 'pstore'
+require_relative 'helpers'
 
 store = PStore.new('my_database.pstore') # Create or Load my_database.pstore file
+
 store.transaction do
   store[:nom] = [ "Name" ]
   store[:booking] = [ "Bookings" ]
@@ -12,54 +14,51 @@ store.transaction do
   store[:secret] = [ "Time." ]
  # Save Changes
   store.commit
-
-
   # Read more at the docs at http://ruby-doc.org/stdlib-2.2.3/libdoc/pstore/rdoc/PStore.html
 end
+
 $times_posted = 0
 
 get '/' do
-$times_posted += 1
-if $times_posted > 13
-'bobe'
-store.transaction{store[:secret]}
-sleep 5
-exit 1
-end
+  $times_posted += 1
+  if $times_posted > 13
+    'bobe'
+    store.transaction{store[:secret]}
+    sleep 5
+    exit 1
+  end
   erb :index
 end
 
 
 post '/' do
-$times_posted += 1
-if $times_posted > 11
-send_file('my_database.pstore')
-sleep 10
-exit 1
-end
-@params = params
-unless params["name"].to_s.include?('@')
-params["name"] = 'That is not an email address!'
-else
-store.transaction do
-  store[:nom] << params["name"].to_s 
-  store[:booking] << params["booking"].to_s
-  store[:ip] << '1.1.1.1'
-  store[:secret] << Time.now.to_s
- # Save Changes
-  store.commit
+  $times_posted += 1
+  if $times_posted > 11
+    send_file('my_database.pstore')
+    sleep 10
+    exit 1
+  end
+  @params = params
 
+  if input_includes_naughty?
+    params["name"] = 'NAAAUGHTY'
 
-  # Read more at the docs at http://ruby-doc.org/stdlib-2.2.3/libdoc/pstore/rdoc/PStore.html
-end
-params["name"] = ''
+  elsif !params["name"].to_s.include?('@')
+    params["name"] = 'That is not an email address!'
 
-
-
-
-end
-@times_posted = $times_posted + 1
-erb :index
-#"My name is #{params[:name]}, and I love #{params[:favorite_food]}"
-
+  else
+    store.transaction do
+      store[:nom] << params["name"].to_s
+      store[:booking] << params["booking"].to_s
+      store[:ip] << '1.1.1.1'
+      store[:secret] << Time.now.to_s
+      # Save Changes
+      store.commit
+      # Read more at the docs at http://ruby-doc.org/stdlib-2.2.3/libdoc/pstore/rdoc/PStore.html
+    end
+    params["name"] = ''
+  end
+  @times_posted = $times_posted + 1
+  erb :index
+  #"My name is #{params[:name]}, and I love #{params[:favorite_food]}"
 end
--- helpers.rb
+++ helpers.rb
@@ -1,11 +1,13 @@
 def input_includes_naughty?
-  naughty_list.any? { |naughty| naughty.match(@params['email'].strip) }
+  p = @params['name']
+  naughty_list.include? p.gsub(/[^0-9A-Za-z]/,'')
 end
 
 def naughty_list
   @_naughty_list ||= begin
     file = File.read('naughty.json')
-    JSON.parse(file)
+    all = JSON.parse(file).to_a
+    all.map { |string| string.gsub(/[^0-9A-Za-z]/, '') }
   end
 end
 
